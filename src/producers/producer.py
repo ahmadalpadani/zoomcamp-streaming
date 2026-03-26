6fcccb7a7167d8f85a44b924ba25c1a8be3cad25
Ahmad Fathoni Alpadani <63529281+ahmadalpadani@users.noreply.github.com>
2026-03-26 18:09:52 +0000

    push 1

---
+++ src/producers/producer.py
@@ -0,0 +1,66 @@
+import dataclasses
+import json
+import sys
+import time
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).parent.parent))
+
+import pandas as pd
+from kafka import KafkaProducer
+from dataclasses import dataclass
+
+# Load data
+url = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2025-11.parquet"
+columns = ['PULocationID', 'DOLocationID', 'trip_distance', 'total_amount', 'tpep_pickup_datetime']
+df = pd.read_parquet(url, columns=columns).head(1000)
+
+# Data model
+@dataclass
+class Ride:
+    PULocationID: int
+    DOLocationID: int
+    trip_distance: float
+    total_amount: float
+    tpep_pickup_datetime: int
+
+# Transform function
+def ride_from_row(row):
+    return Ride(
+        PULocationID=int(row['PULocationID']),
+        DOLocationID=int(row['DOLocationID']),
+        trip_distance=float(row['trip_distance']),
+        total_amount=float(row['total_amount']),
+        tpep_pickup_datetime=int(row['tpep_pickup_datetime'].timestamp() * 1000),
+    )
+
+# Serializer
+def ride_serializer(ride):
+    ride_dict = dataclasses.asdict(ride)
+    json_str = json.dumps(ride_dict)
+    return json_str.encode('utf-8')
+
+# Kafka setup
+server = 'localhost:9092'
+
+producer = KafkaProducer(
+    bootstrap_servers=[server],
+    value_serializer=ride_serializer
+)
+
+# Send data
+t0 = time.time()
+
+topic_name = 'rides'
+
+for _, row in df.iterrows():
+    ride = ride_from_row(row)
+    producer.send(topic_name, value=ride)
+    print(f"Sent: {ride}")
+    time.sleep(0.01)
+
+producer.flush()
+
+t1 = time.time()
+
+print(f'took {(t1 - t0):.2f} seconds')
